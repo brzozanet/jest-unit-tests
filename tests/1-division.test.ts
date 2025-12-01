3c3d1bfd3265a7afac5952041846c5c1d432a6d1
Paweł Brzozowski <pawel@brzoza.net>
2025-12-01 18:39:25 +0100

    Enhance division tests with additional cases and improve test descriptions

--- tests/1-division.test.ts
+++ tests/1-division.test.ts
@@ -1,11 +1,19 @@
 import { division } from "../src/1-division";
 
-test("Test dzielenia", () => {
+test("Dzielenie liczb całkowitych bez reszty", () => {
   const a = 20;
   const b = 4;
   const expected = 5;
 
   const result = division(a, b);
+  expect(result).toBe(expected);
+});
 
+test("Dzielenie przez zero", () => {
+  const a = 5;
+  const b = 0;
+  const expected = Infinity;
+
+  const result = division(a, b);
   expect(result).toBe(expected);
 });
