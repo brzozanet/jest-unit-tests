0ebb65019d8f208dc4d22df20e227ce21ce70e48
Paweł Brzozowski <pawel@brzoza.net>
2025-12-01 19:01:06 +0100

    Remove console log from yearly profit calculator function

--- src/2-profits.ts
+++ src/2-profits.ts
@@ -7,5 +7,3 @@ export function yearlyProfitCalculator(
     startAmount * ((100 + interestRate) / 100 - 1) * ((100 - tax) / 100);
   return parseFloat(profit.toFixed(2));
 }
-
-console.log(yearlyProfitCalculator(10000, 20));
