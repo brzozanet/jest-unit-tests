64b2e4f0fbddc34843d06653c00ec92a2070903e
Paweł Brzozowski <pawel@brzoza.net>
2025-12-01 14:57:50 +0100

    Update package dependencies and implement division test case

--- tests/1-division.test.ts
+++ tests/1-division.test.ts
@@ -1,1 +1,11 @@
-// test
+import { division } from "../src/1-division";
+
+test("Test dzielenia", () => {
+  const a = 20;
+  const b = 4;
+  const expected = 5;
+
+  const result = division(a, b);
+
+  expect(result).toBe(expected);
+});
