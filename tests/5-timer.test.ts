29d14ecf5ae256b32cde8d97d4ec1993d785a6bb
Paweł Brzozowski <pawel@brzoza.net>
2025-12-19 10:47:48 +0100

    Add unit test for cancellableDelay function in 5-timer.test.ts

---
+++ tests/5-timer.test.ts
@@ -0,0 +1,10 @@
+import { cancellableDelay } from "../src/5-timer";
+
+jest.useFakeTimers();
+
+test("Wykonanie funkcji po określonym czasie", () => {
+  const callback = jest.fn();
+  cancellableDelay(3, callback);
+  jest.advanceTimersByTime(3000);
+  expect(callback).toHaveBeenCalledTimes(1);
+});
