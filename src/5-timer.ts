fca26203f787bfc44179406dddaa3856b6844a06
Paweł Brzozowski <pawel@brzoza.net>
2025-12-20 23:55:01 +0100

    Update comments in cancellableDelay function and add commented-out Counter component

--- src/5-timer.ts
+++ src/5-timer.ts
@@ -18,4 +18,22 @@ export function cancellableDelay(timeInSeconds = 5, callback: () => void) {
     clearTimeout(timer);
   };
 }
-// test
+// test 1
+// test 2
+
+// import React, { useState } from 'react';
+
+// export function Counter() {
+//   const [count, setCount] = useState<number>(0);
+
+//   return (
+//     <div>
+//       <h2>Licznik: {count}</h2>
+//       <div>
+//         <button onClick={() => setCount(c => c - 1)}>-</button>
+//         <button onClick={() => setCount(c => c + 1)}>+</button>
+//         <button onClick={() => setCount(0)}>Reset</button>
+//       </div>
+//     </div>
+//   );
+// }
