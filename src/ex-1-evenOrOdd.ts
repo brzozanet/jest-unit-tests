3a1ebb80a72c3288fc37a706e98224ecc6281475
Paweł Brzozowski <pawel@brzoza.net>
2026-01-10 09:33:50 +0100

    Add evenOrOdd function and tests for PLN to USD exchange rate

---
+++ src/ex-1-evenOrOdd.ts
@@ -0,0 +1,7 @@
+export function evenOrOdd(num) {
+  return num % 2 === 0 ? "even" : "odd";
+}
+
+// Twoim zadaniem jest napisanie dwóch testów jednostkowych:
+// Jeden, który sprawdzi czy funkcja poprawnie wykrywa liczby parzyste.
+// Drugi, który sprawdzi czy funkcja poprawnie wykrywa liczby nieparzyste.
